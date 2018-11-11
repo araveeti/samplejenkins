08e7ff8b2f656b97e1d04169115cb2c20ac98fbb
Demo Scotiobank <venkatestscotioa@gmail.com>
2018-11-11 09:56:35 +0530

    COmmit

---
+++ project/src/main/java/com/selenium/acc/project/App.java
@@ -0,0 +1,13 @@
+package com.selenium.acc.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ project/src/test/java/com/selenium/acc/project/AppTest.java
@@ -0,0 +1,23 @@
+package com.selenium.acc.project;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void test() {
+		System.out.println("My Maven Welcome to the Programming");
+		Assert.assertFalse(1==1);
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Enjoy");
+	}
+	
+  
+}
